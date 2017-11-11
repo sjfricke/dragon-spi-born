77cbf5a0516285c18b79b7716c9eb626248d035a
malulleybovo <malulleybovo@wisc.edu>
2017-11-11 17:20:42 -0600

    Added Sprite and Container Add to Renderer + LogMan Improvement
    - Renderer can now add sprites and containers to the app.
    - LogMan now supports '%' notation to print variable information to console by using format: ("msg = %s", myString).

--- website/src/logman.js
+++ website/src/logman.js
@@ -7,16 +7,19 @@ console.log = function () { };
 console.warn = function () { };
 console.error = function () { };
 
-function log(name, msg) {
-    logType('log', name, msg);
+function log() {
+    logType.apply(this, ['log'].concat(
+        Array.prototype.slice.call(arguments)));
 }
 
-function warn(name, msg) {
-    logType('warn', name, msg);
+function warn() {
+    logType.apply(this, ['warn'].concat(
+        Array.prototype.slice.call(arguments)));
 }
 
-function err(name, msg) {
-    logType('error', name, msg);
+function err() {
+    logType.apply(this, ['error'].concat(
+        Array.prototype.slice.call(arguments)));
 }
 
 function logType(type, name, msg) {
@@ -24,10 +27,15 @@ function logType(type, name, msg) {
         && type !== 'warn'
         && type !== 'error') return;
     if (myconsole[type]
-        && typeof name === 'string'
-        && typeof msg === 'string') {
-        (myconsole[type])(
-            ('%cClass ' + name + ':%c ' + msg), 'color: #a6cd94', 'color: grey'
+        && typeof name === 'string') {
+        if (typeof msg !== 'string') {
+            msg = msg + '';
+        }
+        if (typeof msg !== 'string') return;
+        let slicedArgs = Array.prototype.slice.call(arguments, 3);
+        (myconsole[type]).apply(this,
+            [('%cClass ' + name + ':%c ' + msg), 'color: #a6cd94', 'color: grey'].concat(
+            Array.prototype.slice.call(slicedArgs))
             );
     }
 }
